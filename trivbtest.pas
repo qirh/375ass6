8cb64384bbba4c571fbb15687dad667ee4ea2f16
almto3 <almto3@hotmail.com>
2016-12-06 17:24:50 -0600

    mass purge

--- trivbtest.pas
+++
@@ -1,4 +0,0 @@
-program graph1(output);
-var i,lim : integer;
-begin
-end.
--- trivtest.pas
+++
@@ -1,7 +0,0 @@
-{ trivial test program        -- file triv.pas }
-program graph1(output);
-var i:integer; j:real;
-begin
-   i := 3;
-   j := 3
-end.
